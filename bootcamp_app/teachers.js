ce70e7f82d04e8209df354d52aac91db07bb9796
Javin Leslie Lau <javinlau21@gmail.com>
2025-01-07 20:29:56 -0500

    node PG

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,47 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+// my original
+pool
+  .query(
+    `
+    SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+    FROM teachers 
+    JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+    JOIN students ON students.id = assistance_requests.student_id
+    JOIN cohorts ON cohorts.id = students.cohort_id
+    WHERE cohorts.name LIKE '%${process.argv[2]}%'
+    ORDER BY teacher;
+`
+  )
+  .then((res) => {
+    console.log(res.rows[0]);
+    res.rows.forEach((row) => {
+      console.log(
+        `${row.cohort}: ${row.teacher}`
+      );
+    });
+  })
+  .catch ((err) => console.error("query error", err.stack));
+
+// LHL
+// pool.query(`
+//   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+//   FROM teachers
+//   JOIN assistance_requests ON teacher_id = teachers.id
+//   JOIN students ON student_id = students.id
+//   JOIN cohorts ON cohort_id = cohorts.id
+//   WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+//   ORDER BY teacher;
+//   `)
+//   .then(res => {
+//     res.rows.forEach(row => {
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     })
+//   });
